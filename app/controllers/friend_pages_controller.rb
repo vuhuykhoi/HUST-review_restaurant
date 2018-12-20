a18d598a3bbe49a83c0394b9c37b9e00c6c35e52
vuhuyk <20152032@student.hust.edu.vn>
2018-12-20 07:01:18 +0700

    vi

--- app/controllers/friend_pages_controller.rb
+++ app/controllers/friend_pages_controller.rb
@@ -3,6 +3,11 @@ class FriendPagesController < ApplicationController
     @friend_user = User.find(params[:id])
     @followed_users=@friend_user.following
     @follower_users=@friend_user.followers
-    @reviews = @friend_user.reviews
+    @reviews = @friend_user.reviews.paginate(page: params[:page], per_page: 5).order('created_at DESC')
+
+    respond_to do |format|
+        format.html
+        format.js
+    end
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+  	
     @hot_reviews = Review.order('score DESC').limit(4)
     @new_reviews = Review.order('created_at DESC').limit(3)
     @accurate_users = User.all.sort_by{|user| 5-user.accurate}
